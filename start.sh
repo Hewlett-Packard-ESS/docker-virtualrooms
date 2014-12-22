2d0002b6327f244f3458c38022644ca84497d52a
Karl Stoney <karl.stoney@hp.com>
2014-12-22 10:21:49 +0000

    Fixed an issue with XDisplays

--- start.sh
+++ start.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 uid=${uid:-1000}
 gid=${uid:-1000}
-
+DISPLAY=${DISPLAY:-:0}
 export uid=1000 gid=1000
 sudo chown ${uid}:${gid} -R /home/developer
 sudo su root -c 'echo "developer:x:${uid}:${gid}:Developer,,,:/home/developer:/bin/bash" >> /etc/passwd'
 sudo su root -c 'echo "developer:x:${uid}:" >> /etc/group'
 
-/opt/hpvirtualrooms/hpvirtualrooms
+echo "Starting virtual rooms on X$DISPLAY"
+DISPLAY=$DISPLAY /opt/hpvirtualrooms/hpvirtualrooms
